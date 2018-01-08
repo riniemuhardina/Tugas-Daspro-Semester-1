ebbeb7500d15bceb7b909b5ee6e1176c4728a0b9
Rinie Muhardina <34791338+riniemuhardina@users.noreply.github.com>
2018-01-08 22:44:38 +0700

    Add files via upload

---
+++ BilanganGenap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ public class BilanganGenap
+{	
+	public static void main(String args[])
+ {
+	Scanner masuk = new Scanner(System.in);
+	int bil;
+	bil=0;
+	while (bil<=20) {
+		System.out.println(bil);
+		bil+=2;
+		}
+	}
+}
---
+++ UlangDo1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ public class UlangDo1
+{
+	public static void main(String args[])
+ {
+	Scanner masuk = new Scanner(System.in);
+	int bil;
+	bil=1;
+	while (bil<=10) {
+		System.out.println(bil);
+		bil+=2;
+		}
+	}
+}
