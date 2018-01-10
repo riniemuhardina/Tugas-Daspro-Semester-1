9aaef0226a9d0b3bbf1acaa14c07b80e45633a89
Rinie Muhardina <34791338+riniemuhardina@users.noreply.github.com>
2018-01-11 04:25:19 +0700

    Add files via upload

---
+++ OverloadingBenar.java
@@ -0,0 +1,16 @@
+class OverloadingBenar{
+ public void tambah1(){
+  int a=5, b=10;
+  System.out.println("Hasil Pertambahann dari metod tambah1 ke-1 = "+(a+b));
+ }
+ //Metod tambah1 di overloading dengan 2 parameter (int x, int y)
+ public void tambah1(int x, int y){
+  System.out.println("Hasil Pertambahann dari metod tambah1 ke-2 = "+(x+y));
+ }
+ public static void main(String [] args){
+  Pertambahan pp;
+  pp = new Pertambahan();
+  pp.tambah1();//memanggil metod tambah1 ke-1
+  pp.tambah1(5,5);//memanggil metod tambah1 ke-2
+ }
+}
